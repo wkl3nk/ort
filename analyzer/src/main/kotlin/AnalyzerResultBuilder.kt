e98a5367d1028b5516ff1a3e2b35b16aa7ecd1d4
Sebastian Schuberth <sschuberth@gmail.com>
2023-02-22 09:42:51 +0100

    refactor(AnalyzerResultBuilder): Introduce an `addProject()` function
    This is analogous to `addPackages()` and will be used in an upcoming
    test.
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- analyzer/src/main/kotlin/AnalyzerResultBuilder.kt
+++ analyzer/src/main/kotlin/AnalyzerResultBuilder.kt
@@ -80,13 +80,21 @@ class AnalyzerResultBuilder {
             val projectIssues = issues.getOrDefault(existingProject.id, emptyList())
             issues[existingProject.id] = projectIssues + issue
         } else {
-            projects += projectAnalyzerResult.project
+            addProject(projectAnalyzerResult.project)
             addPackages(projectAnalyzerResult.packages)
 
             if (projectAnalyzerResult.issues.isNotEmpty()) {
                 issues[projectAnalyzerResult.project.id] = projectAnalyzerResult.issues
             }
         }
+    }
+
+    /**
+     * Add the given [project] to this builder. This function can be used for projects that have been obtained
+     * independently of a [ProjectAnalyzerResult].
+     */
+    fun addProject(project: Project) = apply {
+        projects += project
     }
 
     /**
