945593ad4b97c415a5d6e74f1ebed77e1ad274ce
Sebastian Schuberth <sebastian@doubleopen.org>
2023-11-09 22:15:02 +0100

    fix(spdx): Do not allow the snippet name to be blank
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- utils/spdx/src/main/kotlin/model/SpdxSnippet.kt
+++ utils/spdx/src/main/kotlin/model/SpdxSnippet.kt
@@ -137,6 +137,10 @@ data class SpdxSnippet(
             }
         }
 
+        require(name.isNotBlank()) {
+            "The name must not be blank."
+        }
+
         require(snippetFromFile.isNotBlank()) {
             "The snippet from file must not be blank."
         }
